e1cbfedf7f184ec045181db25b40dd9fe0d9ade5
gorbunov-ilya <go9630@yandex.ru>
2023-11-29 01:26:42 +0400

    model owner add unresolved files

---
+++ backend/owner/management/commands/dealer_name_load_data.py
@@ -0,0 +1,27 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from dealers.models import DealersNames
+
+
+class Command(BaseCommand):
+    help = 'Загрузка имён дилеров из файла CSV'
+
+    def handle(self, *args, **kwargs):
+        csv_file = 'data/marketing_dealer.csv'
+        with open(csv_file, 'r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file, delimiter=';')
+            next(csv_reader)
+            for row in csv_reader:
+                dealer_id, name = row
+                dealer, created = DealersNames.objects.get_or_create(
+                    dealer_id=dealer_id,
+                    name=name
+                )
+                if created:
+                    self.stdout.write(
+                        self.style.SUCCESS(f'dealer "{name}" загружен.'))
+                else:
+                    self.stdout.write(self.style.SUCCESS(
+                        f'dealer "{name}" уже в базе.'))
