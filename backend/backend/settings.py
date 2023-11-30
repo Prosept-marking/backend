086d5c14a9972791490b8f8b3343604509f4ef1a
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2023-11-30 15:00:17 +0300

    docker compose done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,6 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
