92eb803c991b47801608cff28cffd08d6f3b8a7a
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2023-12-02 10:27:34 +0300

    fix docker, urls, nginx, workflows

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -7,8 +7,8 @@ from .views import (DealerNamesViewSet, DealerProductsViewSet,
 app_name = 'api.v1'
 
 router_v1 = DefaultRouter()
-router_v1.register('delear-names', DealerNamesViewSet, 'dealernames')
-router_v1.register('delear-products', DealerProductsViewSet, 'dealerproducts')
+router_v1.register('dealer-names', DealerNamesViewSet, 'dealernames')
+router_v1.register('dealer-products', DealerProductsViewSet, 'dealerproducts')
 router_v1.register('owner-products', OwnerProductsViewSet, 'ownerproducts')
 router_v1.register(
     'product-relation', ProductRelationViewSet, 'productrelation')
