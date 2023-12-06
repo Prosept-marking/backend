90e1b53fa5868fd09720e62aea695f3addba2a96
gorbunov-ilya <go9630@yandex.ru>
2023-12-06 07:21:53 +0400

    get_related_product_name api for statistics

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (DealerNamesViewSet, DealerProductsViewSet,
+from .views import (ComparisonSallersViewSet, DailyStatisticsViewSet,
+                    DealerNamesViewSet, DealerProductsViewSet,
                     OwnerProductsViewSet, ProductRelationViewSet)
 
 app_name = 'api.v1'
@@ -12,6 +13,11 @@ router_v1.register('dealer-products', DealerProductsViewSet, 'dealerproducts')
 router_v1.register('owner-products', OwnerProductsViewSet, 'ownerproducts')
 router_v1.register(
     'product-relation', ProductRelationViewSet, 'productrelation')
+router_v1.register(
+    'daily-statistic', DailyStatisticsViewSet, 'dailystatistic')
+router_v1.register(
+    'dealers-statistic', ComparisonSallersViewSet, 'dealersstatistic')
+
 
 urlpatterns = (
     path('', include(router_v1.urls)),
